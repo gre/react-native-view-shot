ae17afadca4dadbfe3ea01c963ff459181a56f12
Ryan Linton <ryan@infinite.red>
2017-03-07 15:53:48 -0800

    Add UWP Support

--- windows/RNViewShot/RNViewShotModule.cs
+++ windows/RNViewShot/RNViewShotModule.cs
@@ -4,8 +4,6 @@ using ReactNative.UIManager;
 using System;
 using System.IO;
 using System.Collections.Generic;
-using Windows.ApplicationModel.Core;
-using Windows.UI.Core;
 
 namespace RNViewShot
 {
@@ -20,7 +18,7 @@ namespace RNViewShot
         /// <summary>
         /// Instantiates the <see cref="RNViewShotModule"/>.
         /// </summary>
-        public RNViewShotModule(ReactContext reactContext) : base(reactContext) 
+        public RNViewShotModule(ReactContext reactContext) : base(reactContext)
         {
             this._reactContext = reactContext;
         }
@@ -40,45 +38,20 @@ namespace RNViewShot
         public void takeSnapshot(int tag, JObject options, IPromise promise)
         {
             string format = options["format"] != null ? options.Value<string>("format") : "png";
-            //Bitmap.CompressFormat compressFormat =
-            //        format.equals("png")
-            //                ? Bitmap.CompressFormat.PNG
-            //                : format.equals("jpg") || format.equals("jpeg")
-            //                ? Bitmap.CompressFormat.JPEG
-            //                : format.equals("webm")
-            //                ? Bitmap.CompressFormat.WEBP
-            //                : null;
-            //if (compressFormat == null)
-            //{
-            //    promise.reject(ErrorUnableToSnapshot, "Unsupported image format: " + format + ". Try one of: png | jpg | jpeg");
-            //    return;
-            //}
             double quality = options["quality"] != null ? options.Value<double>("quality") : 1.0;
             int? width = options["width"] != null ? options.Value<int?>("width") : null;
             int? height = options["height"] != null ? options.Value<int?>("height") : null;
             string result = options["result"] != null ? options.Value<string>("result") : "file";
-            bool snapshotContentContainer = options["snapshotContentContainer"] != null ? options.Value<bool>("snapshotContentContainer") : false;
+            string path = options["path"] != null ? options.Value<string>("path") : null;
+
+            if (format != "png" && format != "jpg" && format != "jpeg")
+            {
+                promise.Reject(ViewShot.ErrorUnableToSnapshot, "Unsupported image format: " + format + ". Try one of: png | jpg | jpeg");
+                return;
+            }
 
             UIManagerModule uiManager = this._reactContext.GetNativeModule<UIManagerModule>();
-            uiManager.AddUIBlock(new ViewShot(tag));
-
-            //try
-            //{
-            //    string name = options["filename"] != null ? options.Value<string>("filename") : null;
-            //    //File tmpFile = "file" == result ? createTempFile(this._reactContext, format, name) : null;
-            //    UIManagerModule uiManager = this._reactContext.GetNativeModule<UIManagerModule>();
-            //    //uiManager.addUIBlock(new ViewShot(tag, format, compressFormat, quality, width, height, tmpFile, result, snapshotContentContainer, promise));
-            //}
-            //catch (Exception e)
-            //{
-            //    promise.reject(ErrorUnableToSnapshot, "Failed to snapshot view tag " + tag);
-            //}
-
-            //Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-            //Graphics graphics = Graphics.FromImage(bitmap as Image);
-            //graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
-            //bitmap.Save("c:\\screenshot.jpeg", ImageFormat.Jpeg);
-            promise.Resolve("Format: " + format + " Quality: " + quality);
+            uiManager.AddUIBlock(new ViewShot(tag, format, quality, width, height, path, result, promise));
         }
     }
 }
