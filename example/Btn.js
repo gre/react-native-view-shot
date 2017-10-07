cbffb12449523360232201f1f2bbd67ce10ee0ea
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-10-07 18:05:59 +0200

    update example with latest RN

--- example/Btn.js
+++ example/Btn.js
@@ -1,13 +1,7 @@
 //@flow
-import React, {
-  Component,
-  PropTypes,
-} from "react";
-import {
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   root: {
@@ -17,22 +11,20 @@ const styles = StyleSheet.create({
     color: "#36f",
     borderWidth: 1,
     borderColor: "#36f",
-    fontSize: 12,
+    fontSize: 12
   }
 });
 
 export default class Btn extends Component {
   static propTypes = {
     onPress: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired
   };
-  render () {
+  render() {
     const { onPress, label } = this.props;
     return (
       <TouchableOpacity onPress={onPress}>
-        <Text style={styles.root}>
-          {label}
-        </Text>
+        <Text style={styles.root}>{label}</Text>
       </TouchableOpacity>
     );
   }
