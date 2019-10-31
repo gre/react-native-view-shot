21942730ab0602bc0d2a30e299283f11f1c560cb
Steve Saul <sjsaul1101@gmail.com>
2019-10-31 08:14:27 -0700

    Update RNViewShot.m

--- ios/RNViewShot.m
+++ ios/RNViewShot.m
@@ -79,7 +79,7 @@ RCT_EXPORT_METHOD(captureRef:(nonnull NSNumber *)target
         reject(RCTErrorUnspecified, [NSString stringWithFormat:@"snapshotContentContainer can only be used on a RCTScrollView. instead got: %@", view], nil);
         return;
       }
-      RCTScrollView* rctScrollView = view;
+      RCTScrollView *view = (RCTScrollView*)[self.bridge.uiManager viewForReactTag:reactTag];
       scrollView = rctScrollView.scrollView;
       rendered = scrollView;
     }
