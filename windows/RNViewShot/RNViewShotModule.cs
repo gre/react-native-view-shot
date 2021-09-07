f7325db47999513de3c0233a1482b5b19809501d
Matteo Pagani <matteo.pagani@microsoft.com>
2021-09-07 12:08:10 +0200

    Added UI dispatcher usage in the Windows implementation

--- windows/RNViewShot/RNViewShotModule.cs
+++ windows/RNViewShot/RNViewShotModule.cs
@@ -1,6 +1,7 @@
 using Microsoft.ReactNative;
 using Microsoft.ReactNative.Managed;
 using System;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 
 namespace RNViewShot
@@ -27,7 +28,7 @@ namespace RNViewShot
         }
 
         [ReactMethod]
-        public async void captureRef(int tag, JSValue options, IReactPromise<string> promise)
+        public async Task<string> captureRef(int tag, JSValue options)
         {
             string format = options["format"].IsNull ? "png" : options["format"].AsString();
             double quality = options["quality"].IsNull ? 1.0 : options["quality"].AsDouble();
@@ -38,22 +39,34 @@ namespace RNViewShot
 
             if (format != "png" && format != "jpg" && format != "jpeg")
             {
-                promise.Reject(new ReactError { Code = ViewShot.ErrorUnableToSnapshot, Message = "Unsupported image format: " + format + ". Try one of: png | jpg | jpeg" });
-                return;
+                return "Unsupported image format: " + format + ". Try one of: png | jpg | jpeg";
             }
 
             try
             {
                 var control = XamlUIService.FromContext(_context.Handle).ElementFromReactTag(tag) as FrameworkElement;
-
-                ViewShot view = new ViewShot();
-                var output = await view.Execute(control, format, quality, width, height, path, result);
-                promise.Resolve(output);
+                string imagePath = await GenerateViewShot(control, format, quality, width, height, path, result);
+                return imagePath;
             }
             catch (Exception exc)
             {
-                promise.Reject(new ReactError { Message = exc.Message, Exception = exc });
+                return exc.Message;
             }
+        }
+
+        private async Task<string> GenerateViewShot(FrameworkElement control, string format, double quality, int width, int height, string path, string result)
+        {
+            TaskCompletionSource<string> tcs = new TaskCompletionSource<string>();
+
+            _context.Handle.UIDispatcher.Post(async () =>
+            {
+                ViewShot view = new ViewShot();
+                var output = await view.Execute(control, format, quality, width, height, path, result);
+                tcs.SetResult(output);
+            });
+
+            var imagePath = await tcs.Task;
+            return imagePath;
         }
     }
 }
