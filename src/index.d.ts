a990eeda889fbb8316693b55581838e97398caf2
steo-ml <80666855+steo-ml@users.noreply.github.com>
2022-06-21 16:45:10 +0800

    add fileName typing to Capture options (#423)
    - update fileName description in README

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,6 +14,10 @@ declare module 'react-native-view-shot' {
 
     export interface CaptureOptions {
         /**
+         * (Android only) the file name of the file. Must be at least 3 characters long.
+         */
+        fileName?: string;
+        /**
          * (number): the width and height of the final image (resized from the View bound. don't provide it if you want
          * the original pixel size).
          */
