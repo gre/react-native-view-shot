9083909a0d97cd1b52629d47e64ab068c6559281
Christoph Pader <christoph@margelo.io>
2023-06-07 10:46:15 +0200

    fix: index.web.js

--- example/Example/index.web.js
+++ example/Example/index.web.js
@@ -3,11 +3,11 @@
  */
 
 import { AppRegistry } from "react-native";
-import App from "./src/App";
+import { App } from "./src/App";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.runApplication("ViewShotExample", {
+AppRegistry.runApplication(appName, {
   rootTag: document.getElementById("root"),
 });
