da7b8f6e27b06f1b9c95f87190118c2ad70b4cfd
Mike Hamilton <mike@mikehamilton.ca>
2025-02-12 15:26:40 +0100

    Fix: releaseCapture throwing error about tmpfile not implemented on web even when using base64 or file result

--- src/RNViewShot.web.js
+++ src/RNViewShot.web.js
@@ -3,8 +3,10 @@ import html2canvas from "html2canvas";
 
 async function captureRef(view, options) {
   if (options.result === "tmpfile") {
-    console.warn("Tmpfile is not implemented for web. Try base64 or file.\n"+
-                 "For compatibility, it currently returns the same result as data-uri");
+    console.warn(
+      "Tmpfile is not implemented for web. Try base64 or file.\n" +
+        "For compatibility, it currently returns the same result as data-uri"
+    );
   }
 
   // TODO: implement snapshotContentContainer option
@@ -14,17 +16,27 @@ async function captureRef(view, options) {
 
   if (options.width && options.height) {
     // Resize result
-    const resizedCanvas = document.createElement('canvas');
-    const resizedContext = resizedCanvas.getContext('2d');
+    const resizedCanvas = document.createElement("canvas");
+    const resizedContext = resizedCanvas.getContext("2d");
     resizedCanvas.height = options.height;
     resizedCanvas.width = options.width;
-    resizedContext.drawImage(renderedCanvas, 0, 0, resizedCanvas.width, resizedCanvas.height);
+    resizedContext.drawImage(
+      renderedCanvas,
+      0,
+      0,
+      resizedCanvas.width,
+      resizedCanvas.height
+    );
     renderedCanvas = resizedCanvas;
   }
 
-  const dataUrl = renderedCanvas.toDataURL("image/" + options.format, options.quality);
-  if (options.result === "data-uri" || options.result === "tmpfile") return dataUrl;
-  return dataUrl.replace(/data:image\/(\w+);base64,/, '');
+  const dataUrl = renderedCanvas.toDataURL(
+    "image/" + options.format,
+    options.quality
+  );
+  if (options.result === "data-uri" || options.result === "tmpfile")
+    return dataUrl;
+  return dataUrl.replace(/data:image\/(\w+);base64,/, "");
 }
 
 function captureScreen(options) {
@@ -32,11 +44,13 @@ function captureScreen(options) {
 }
 
 function releaseCapture(uri) {
-  throw new Error("Tmpfile is not implemented for web. Try base64 or file");
+  if (options.result === "tmpfile") {
+    throw new Error("Tmpfile is not implemented for web. Try base64 or file");
+  }
 }
 
 export default {
   captureRef,
   captureScreen,
-  releaseCapture
-}
+  releaseCapture,
+};
